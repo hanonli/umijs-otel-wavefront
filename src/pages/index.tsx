05f5e6199c85cf12a1e469e397ebdf66bb3ac6bc
Somruethai Onamphai <Somruethai@got.co.th>
2023-07-07 15:37:03 +0700

    add otel config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import yayJpg from '../assets/yay.jpg';
+import TraceProvider from '../tracing.js';
 
 export default function HomePage() {
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
+    <TraceProvider>
+      <div>
+        <h2>Yay! Welcome to umi!</h2>
+        <p>
+          <img src={yayJpg} width="388" />
+        </p>
+        <p>
+          To get started, edit <code>pages/index.tsx</code> and save to reload.
+        </p>
+      </div>
+    </TraceProvider>
   );
 }
